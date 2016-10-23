8ea6985dacc5bdb87f2a7374d7b5fea27f964f82
Kenton Lam <kenton.lam02@outlook.com>
2016-10-23 11:49:56 +1000

    Added WCF communication.

---
+++ StudyLockerApp/StudyLockerWCF/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyLockerWCF
+{
+    public class Class1
+
+    {
+        public decimal test { get; set; }
+    }
+}
---
+++ StudyLockerApp/WCFTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<StudyLockerWCF.IStudyLockerWCF> pipeFactory = new ChannelFactory<StudyLockerWCF.IStudyLockerWCF>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress("net.pipe://localhost"));
+
+            StudyLockerWCF.IStudyLockerWCF pipeProxy = pipeFactory.CreateChannel();
+
+            while (true)
+            {
+                Console.Write("\n: ");
+                Console.WriteLine(pipeProxy.DecimalTest(new Decimal(Convert.ToInt32(Console.ReadLine()))));
+
+            }
+        }
+    }
+}
